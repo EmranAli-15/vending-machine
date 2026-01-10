0b4736c15190a4c1831c50ca231be0cc5fa95e97
EmranAli-15 <mohammademranali15@gmail.com>
2026-01-10 15:28:08 +0600

    working on svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Container from "./ui/Container"
+import machine from "./assets/machine.png"
+import { Coke, Drinko, Lemon, Mojo, Potato } from "./ui/Icons"
 
 function App() {
 
   return (
-    <div>
-      <h1>This is my vending machine</h1>
-    </div>
+    <Container>
+      <h1 className="bg-1">This is my vending machine</h1>
+
+
+      <div className="grid grid-cols-3">
+        <div className="col-span-1">
+          <img src={machine} alt="" />
+        </div>
+        <div className="col-span-2">
+          <div className="grid grid-cols-4 gap-4">
+            <div className="bg-1">
+              <Coke w={150}></Coke>
+            </div>
+            <div className="bg-1">
+              <Mojo w={150}></Mojo>
+            </div>
+            <div className="bg-1">
+              <Lemon w={150}></Lemon>
+            </div>
+            <div className="bg-1">
+              <Drinko w={150}></Drinko>
+            </div>
+            <div className="bg-1">
+              <div className="relative">
+                <Potato w={110}></Potato>
+                <h1 className="absolute top-10 left-7 text-white font-bold -rotate-[30deg]">Potato</h1>
+              </div>
+            </div>
+            <div className="bg-1">1</div>
+            <div className="bg-1">1</div>
+            <div className="bg-1">1</div>
+          </div>
+        </div>
+      </div>
+    </Container>
   )
 }
 
---
+++ src/ui/Container.tsx
@@ -0,0 +1,11 @@
+import { type ReactNode } from 'react'
+
+export default function Container({ children }: { children: ReactNode }) {
+    return (
+        <div className='max-w-7xl mx-auto'>
+            <div className='m-1'>
+                {children}
+            </div>
+        </div>
+    )
+}
